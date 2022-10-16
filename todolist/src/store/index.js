2ec31df892b9b9fd38b5b742669e06c25537cec9
Lu-syuan <as6552783@gmail.com>
2022-10-17 03:10:00 +0800

    read

--- todolist/src/store/index.js
+++ todolist/src/store/index.js
@@ -1,17 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import localStorage from '../modules/LocalStorage'
 
+const STORE = localStorage('todo-vue')
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: []
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    SET_TODOS (state) {
+      state.todos = STORE.load()
+    }
   },
   actions: {
+    READ_TODOS ({ commit }) {
+      // 1.GET
+      const todos = STORE.load()
+      // 2. commit mutations
+      commit('SET_TODOS')
+      // 3.return
+      return {
+        todos
+      }
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
